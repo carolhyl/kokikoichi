ec1b62c71e1c9e168cad5b428905e3256ee1446c
caro1 <waxapple666@gmail.com>
2025-04-12 19:08:06 +0800

    feat: enable auto deploy

--- kokikoichi/shopping-web/next.config.ts
+++ kokikoichi/shopping-web/next.config.ts
@@ -1,8 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  output: 'export',
+  basePath: '/kokikoichi',
+  assetPrefix: '/kokikoichi',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
